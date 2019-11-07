239d5e665b15495ce4cdb9ced17c52bdb5e529c8
alecseperack17 <55203347+alecseperack17@users.noreply.github.com>
2019-11-07 16:04:48 -0600

    Create Data Statements.sql

---
+++ Data Statements.sql
@@ -0,0 +1,9 @@
+INSERT INTO Departments (Profit, Sales, Reviews, Employees, Production,)
+VALUES ('100000', '200000', 100, 5000, 250,);
+
+INSERT INTO Locatioins (Production, Warehousing, TireType, Profit,)
+VALUES ('1', '2000', 100, 100000,);
+
+INSERT INTO Products (CostToProduce, Materials, TireType, Profit,)
+VALUES ('100000', '200000', 100, 5000, 250,);
+
