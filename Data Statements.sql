c6f90b92414e37aa3f0138d6115136f3182d6fbf
Conner Hemming <55203349+ConnerHemming@users.noreply.github.com>
2019-11-08 01:24:31 -0600

    Update Data Statements.sql

--- Data Statements.sql
+++ Data Statements.sql
@@ -10,6 +10,7 @@ VALUES ('100000', '200000', 100, 5000, 250,);
 INSERT INTO Employees ( EmployeeId, Email, Phone, HireDate, Salary, Sales)
 Values ('11686', "1234@1234.com', '123-456-7890', '10-21-17', '23000')
         
+        
 Insert Into Projects (ProjectID, StartDate, CompletionDate, EmployeeID, CustomerID)
 Values ('12345', '06-07-15', '06-07-16', '11686', '4321')
         
