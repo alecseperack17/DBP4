4f533a305d3dd37dea3077c59f988312b7200789
Julian Torres <55203447+jtorres6899@users.noreply.github.com>
2019-11-07 22:45:45 -0600

    Update Data Statements.sql

--- Data Statements.sql
+++ Data Statements.sql
@@ -7,3 +7,9 @@ VALUES ('1', '2000', 100, 100000,);
 INSERT INTO Products (CostToProduce, Materials, TireType, Profit,)
 VALUES ('100000', '200000', 100, 5000, 250,);
 
+INSERT INTO Employees ( EmployeeId, Email, Phone, HireDate, Salary, Sales)
+Values ('11686', "1234@1234.com', '123-456-7890', '10-21-17', '23000')
+        
+Insert Into Projects (ProjectID, StartDate, CompletionDate, EmployeeID, CustomerID)
+Values ('12345', '06-07-15', '06-07-16', '11686', '4321')
+        
