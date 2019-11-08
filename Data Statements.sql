71d4f12970766ede0ecc629b0d9713ed3a1cfe12
Conner Hemming <55203349+ConnerHemming@users.noreply.github.com>
2019-11-08 01:25:58 -0600

    Update Data Statements.sql

--- Data Statements.sql
+++ Data Statements.sql
@@ -1,12 +1,12 @@
 INSERT INTO Departments (Profit, Sales, Reviews, Employees, Production,)
-VALUES ('100000', '200000', 100, 5000, 250,);
+VALUES ('100000', '200000', 100, 5000, 250,)
+
 
 INSERT INTO Locatioins (Production, Warehousing, TireType, Profit,)
 VALUES ('1', '2000', 100, 100000,);
 
 INSERT INTO Products (CostToProduce, Materials, TireType, Profit,)
 VALUES ('100000', '200000', 100, 5000, 250,);
-
 
 INSERT INTO Employees ( EmployeeId, Email, Phone, HireDate, Salary, Sales)
 Values ('11686', "1234@1234.com', '123-456-7890', '10-21-17', '23000')
